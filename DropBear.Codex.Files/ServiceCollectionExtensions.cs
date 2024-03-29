84bfba284d3713f82399ba3a21936de83d3c77e3
DropBear <tkuchel@users.noreply.github.com>
2024-03-29 16:49:54 +1100

    Updated the ServiceCollectionExtensions.cs to add the missing file manager interfaces and implementations.

--- DropBear.Codex.Files/ServiceCollectionExtensions.cs
+++ DropBear.Codex.Files/ServiceCollectionExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using DropBear.Codex.AppLogger.Extensions;
 using DropBear.Codex.Files.Interfaces;
+using DropBear.Codex.Files.Models.FileComponents;
+using DropBear.Codex.Files.Models.FileComponents.SubComponents;
 using DropBear.Codex.Files.Services;
 using DropBear.Codex.Serialization;
 using DropBear.Codex.Utilities.Extensions;
@@ -32,6 +34,11 @@ public static class ServiceCollectionExtensions
         services.AddDataSerializationServices();
 
         // Add file manager service
+        services.AddTransient<ICompressionSettings, CompressionSettings>();
+        services.AddTransient<IFileHeader, FileHeader>();
+        services.AddTransient<IFileMetaData, FileMetaData>();
+        services.AddTransient<IContentContainer, ContentContainer>();
+        services.AddTransient<IFileContent, FileContent>();
         services.AddTransient<IFileManager, FileManager>();
 
         // Add hashing utilities
